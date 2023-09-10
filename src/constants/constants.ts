e3eada707527197587125db267950a59a38e4906
devseop <cocc263@gmail.com>
2023-09-10 23:02:15 +0900

    :art: FEAT: 차트 커스텀 옵션 설정 및 호버시 데이터 툴팁 구현

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -8,3 +8,5 @@ export const COLORS = {
   red: { base: 'rgba(251, 93, 93, 1)', dimmed: 'rgba(251, 93, 93, 0.2)' },
   blue: { base: 'rgba(46, 106, 254, 1)', dimmed: 'rgba(46, 106, 254, 0.4)' },
 } as const;
+
+export const AREA_CHART_SCALE_MAX = 200; //bar 차트와 겹치지 않도록 하기 위함
---
+++ src/utls/customedChartOption.ts
@@ -0,0 +1,43 @@
+import { TooltipItem } from 'chart.js';
+import { AREA_CHART_SCALE_MAX, X_AXIS_KEY } from '../constants/constants';
+import { ITimeSeriesData } from '../types/types';
+
+interface ICustomedChartsTooltip extends TooltipItem<'bar' | 'line'> {
+  raw: ITimeSeriesData;
+}
+
+export const customedChartOptions = {
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  scales: {
+    area: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const,
+      title: { display: true, text: 'Area' },
+      max: AREA_CHART_SCALE_MAX,
+    },
+    bar: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      title: { display: true, text: 'Bar' },
+    },
+  },
+  parsing: {
+    xAxisKey: X_AXIS_KEY,
+  },
+  plugins: {
+    tooltip: {
+      callbacks: {
+        afterTitle: function (tooltipItems: ICustomedChartsTooltip[]) {
+          const [item] = tooltipItems;
+          return `ID: ${item.raw.y.id}`;
+        },
+      },
+    },
+  },
+};
