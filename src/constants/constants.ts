3d9b80d31e78a1fdf9b116e048a7b5fb2120c697
devseop <cocc263@gmail.com>
2023-09-10 22:00:50 +0900

    FEAT: 상수명 변경 및 추가 상수 설정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,10 @@
-export const VALUE_AREA = 'value_area';
-export const VALUE_BAR = 'value_bar';
+export const VALUE_AREA_KEY = 'value_area';
+export const VALUE_BAR_KEY = 'value_bar';
 
-export const X_AXIS = 'x';
-export const Y_AXIS = 'y';
+export const X_AXIS_KEY = 'x';
+export const Y_AXIS_KEY = 'y';
+
+export const COLORS = {
+  red: { base: 'rgba(251, 93, 93, 1)', dimmed: 'rgba(251, 93, 93, 0.2)' },
+  blue: { base: 'rgba(46, 106, 254, 1)', dimmed: 'rgba(46, 106, 254, 0.4)' },
+} as const;
