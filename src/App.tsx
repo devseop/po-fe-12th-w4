1b25d67f13b47f16a4d64885c97278781560b4d4
devseop <cocc263@gmail.com>
2023-09-13 00:58:04 +0900

    FEAT: spinner 추가 및 렌더링 방식 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,30 @@ import { useData } from './hooks/useData';
 import './utils/registerChartJS';
 import { customedChartOptions } from './utils/customedChartOption';
 import { ITimeSeriesData } from './types/types';
+import { Spinner } from './components/Spinner';
 
 export interface IChartDataState extends ChartData<'bar' | 'line', ITimeSeriesData[]> {}
 
 const App = () => {
   const { chartData, isLoading, uniqueIds, chartRef, onClick } = useData();
 
-  if (isLoading) {
-    return (
-      <Loading>
-        <p>Loading...</p>
-      </Loading>
-    );
-  }
-
   return (
     <>
       <Container>
         <ChartHeader />
-        <FilterButtons uniqueIds={uniqueIds} />
-        <TimeSeriesChart
-          chartData={chartData}
-          chartOptions={customedChartOptions}
-          ref={chartRef}
-          onClick={onClick}
-        />
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <FilterButtons uniqueIds={uniqueIds} />
+            <TimeSeriesChart
+              chartData={chartData}
+              chartOptions={customedChartOptions}
+              ref={chartRef}
+              onClick={onClick}
+            />
+          </>
+        )}
       </Container>
     </>
   );
@@ -43,14 +42,6 @@ const Container = styled.div`
   max-width: 1280px;
   padding: 0 20px;
   margin: 80px auto 0;
-`;
-
-const Loading = styled.div`
-  margin: 350px auto 0;
-  text-align: center;
-  font-size: 24px;
-  font-weight: 600;
-  color: white;
 `;
 
 export default App;
