675a3e7bfac0c5333c2909e06bd6529d7c138483
devseop <cocc263@gmail.com>
2023-09-12 19:43:03 +0900

    RENAME/COMMENT: 파일명 변경 및 해당 파일 내 주석 추가

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -6,16 +6,17 @@ import { getUniqueIds } from '../utils/getUniqueIds';
 
 export const useChartData = () => {
   const [chartData, setchartData] = useState<IChartDataState>({ datasets: [] });
+  const [uniqueIds, setUniqueIds] = useState<string[]>([]); //데이터별 id: 성북구, 강남구, 노원구, 중랑구
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [uniqueIds, setUniqueIds] = useState<string[]>([]);
 
+  // API 호출, res를 이용하여 chartData, uniqueIds 설정
   useEffect(() => {
     const laodData = async () => {
       try {
         setIsLoading(true);
         const res = await fetchData();
-        setchartData(convertChartData(res) as IChartDataState);
         setUniqueIds(getUniqueIds(res));
+        setchartData(convertChartData(res) as IChartDataState);
         setIsLoading(false);
       } catch (err) {
         console.error(err);
@@ -25,5 +26,5 @@ export const useChartData = () => {
     laodData();
   }, []);
 
-  return { chartData, isLoading, uniqueIds };
+  return { chartData, uniqueIds, isLoading };
 };
