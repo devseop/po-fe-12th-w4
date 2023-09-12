7c15a5b15f6154d9ecef23e9ca90c51cbd1a1c9a
devseop <cocc263@gmail.com>
2023-09-12 19:45:14 +0900

    FEAT: 라우터 및 글로벌 스타일 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './style.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Global } from '@emotion/react';
+import { globalStyles } from './styles/globalStyles';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Global styles={globalStyles} />
+    <App />
+  </BrowserRouter>,
+);
